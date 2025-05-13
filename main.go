5eaa23149a3d2beeb1927f4d340c2a7943ba0e06
Yash Tiwari <tiwari.yash@zomato.com>
2025-05-13 15:11:23 +0530

    STEP 2: Setting up structure of Task

--- main.go
+++ main.go
@@ -6,6 +6,16 @@ import (
 	"net/http"
 )
 
+type Task struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+}
+
+var tasks []Task
+var nextID int = 1
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Welcome to the Go To-Do CRUD API!")
